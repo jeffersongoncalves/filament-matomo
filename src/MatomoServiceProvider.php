084e061e9f8265952f765e8895b14305d697c48d
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2026-02-22 19:53:37 -0300

    refactor: move render hook back to ServiceProvider for backward compatibility

--- src/MatomoServiceProvider.php
+++ src/MatomoServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace JeffersonGoncalves\Filament\Matomo;
 
+use Filament\Support\Facades\FilamentView;
+use Filament\View\PanelsRenderHook;
+use Illuminate\Contracts\View\View;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -11,5 +14,10 @@ class MatomoServiceProvider extends PackageServiceProvider
     {
         $package->name('filament-matomo')
             ->hasTranslations();
+    }
+
+    public function packageRegistered(): void
+    {
+        FilamentView::registerRenderHook(PanelsRenderHook::HEAD_END, fn (): View => view('matomo::script'));
     }
 }
